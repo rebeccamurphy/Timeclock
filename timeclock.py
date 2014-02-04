abc365fa746c49dcfc830b876563d372eeed8112
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-02-04 16:07:52 -0500

    fixed up timezone thing

--- timeclock.py
+++ timeclock.py
@@ -1,5 +1,14 @@
-from time import gmtime, strftime
-time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+import time
+currTime = time.ctime()
 
+def linecount(filename):
+    count = 0
+    for x in open(filename):
+        count += 1
+    return count
+count = linecount('hours.txt')
 with open("hours.txt", "a") as text_file: # writes out the urls to hours.txt 
-   text_file.write(str.format("%s\n" % time))
+	if (count %2 ==0):
+   		text_file.write("IN:" + str.format("%s\n" % currTime))
+   	else:
+   		text_file.write("OUT:" + str.format("%s\n" % currTime))
